1837674675fdc658b8dd5d35e4646f631c9d1234
bencoll <bwcollins97@gmail.com>
2020-01-06 18:20:59 +1100

    Make new post page accessible for testing

--- app.py
+++ app.py
@@ -22,5 +22,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/new')
+def new_post():
+    return render_template('post.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
