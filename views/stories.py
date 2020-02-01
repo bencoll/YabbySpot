d180e339aad5251b1eb394bc2b8a0438d681ab5a
bencoll <bwcollins97@gmail.com>
2020-02-01 22:58:39 +1100

    Add new-post page

--- views/stories.py
+++ views/stories.py
@@ -1,9 +1,20 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, render_template
 
 story_blueprint = Blueprint('stories', __name__)
 
 
 @story_blueprint.route('/')
 def index():
+    """
+    Collect list of stories and display them as cards to be selected
+    :return: stories page with list of stories
+    """
+    # TODO: Stories landing page
     return 'stories.index'
+
+
+@story_blueprint.route('/new', methods=['GET', 'POST'])
+def new_story():
+    if request.method == 'POST':
+        pass
+    return render_template("new_post.html", post="Story")
